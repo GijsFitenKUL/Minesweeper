e43931c03214d869a089519b670a0392bc169b1e
toonmostinckx <80100676+toonmostinckx@users.noreply.github.com>
2021-03-05 14:15:45 +0100

    initialize() en shuffleArray() moeten ng is getest worden. mss handig voor een main klasse aan te maken voor methodes te testen.

--- src/Minesweeper/Board.java
+++ src/Minesweeper/Board.java
@@ -3,18 +3,44 @@ package Minesweeper;
 //hey
 // hallokes
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.Arrays;
 
 public class Board implements BoardInterface{
     private int Width;
     private int Height;
     private int nrOfBombs;
     public ArrayList<Cell> currentBoard;
+    private int[] randomXPostition;
+    private int[] randomYPosition;
 
     public Board(int Width, int Height, int nrOfBombs){
         this.Width = Width;
         this.Height = Height;
         this.nrOfBombs = nrOfBombs;
         this.currentBoard = new ArrayList<Cell>(Height * Width);
+        randomYPosition = new int[Height];
+        randomXPostition = new int[Width];
+    }
+
+    //initialize and shuffleArray() moeten ng getest worden
+    public void initialize(){
+        for(int i = 0; i < getNrOfBombs(); i++){
+            randomXPostition[i] = i;
+            randomYPosition[i] = i;
+        }
+        shuffleArray(randomXPostition);
+        shuffleArray(randomYPosition);
+    }
+
+    public void shuffleArray(int[] array){
+        Random r= new Random();
+        for(int i = 0; i < array.length; i++){
+            int randomIndex = r.nextInt(array.length);
+            int tempValue = array[randomIndex];
+            array[randomIndex] = array[i];
+            array[i] = tempValue;
+        }
     }
 
     @Override
