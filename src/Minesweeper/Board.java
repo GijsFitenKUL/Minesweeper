d1be6c68e52e995d44980426c1a880e27a3d348f
GijsFitenKUL <78799069+GijsFitenKUL@users.noreply.github.com>
2021-03-05 12:08:57 +0100

    Board Interface updated

--- src/Minesweeper/Board.java
+++ src/Minesweeper/Board.java
@@ -1,6 +1,4 @@
 package Minesweeper;
 
 public class Board implements BoardInterface{
-    //zie je dit gijs?
-    //Ik ziet dit!
 }
--- src/Minesweeper/BoardInterface.java
+++ src/Minesweeper/BoardInterface.java
@@ -1,4 +1,15 @@
 package Minesweeper;
 
+import java.util.ArrayList;
+
 public interface BoardInterface {
+   void initialise();
+   int getWidth();
+   void setWidth();
+   int getHeight();
+   void setHeight();
+   int getNrOfBombs();
+   void setNrOfBombs();
+   ArrayList<ArrayList<Integer>> getCurrentState();
+   void setCurrentState();
 }
