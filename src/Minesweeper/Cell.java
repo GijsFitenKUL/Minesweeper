3b17ea435058418751a9accd036a830e3e60be41
GijsFitenKUL <78799069+GijsFitenKUL@users.noreply.github.com>
2021-03-05 12:45:39 +0100

    Added cell methods

--- src/Minesweeper/Cell.java
+++ src/Minesweeper/Cell.java
@@ -5,4 +5,33 @@ public class Cell implements CellInterface {
     private boolean hidden;
     private boolean isFlag;
 
+    public Cell(){
+        this.hidden = true;
+        this.isFlag = false;
+    }
+
+    @Override
+    public void setFlag() {
+        this.isFlag = true;
+    }
+
+    @Override
+    public boolean isFlag() {
+        return isFlag;
+    }
+
+    @Override
+    public void reveal() {
+        this.hidden = false;
+    }
+
+    @Override
+    public boolean isHidden() {
+        return hidden;
+    }
+
+    @Override
+    public void unsetFlag() {
+        this.isFlag = false;
+    }
 }
--- src/Minesweeper/CellInterface.java
+++ src/Minesweeper/CellInterface.java
@@ -1,4 +1,9 @@
 package Minesweeper;
 
 public interface CellInterface {
+    void setFlag();
+    boolean isFlag();
+    void reveal();
+    boolean isHidden();
+    void unsetFlag();
 }
