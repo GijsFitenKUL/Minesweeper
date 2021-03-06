b5451da995a3930f5ef5ebe0db36690a94dd935d
GijsFitenKUL <78799069+GijsFitenKUL@users.noreply.github.com>
2021-03-06 14:00:53 +0100

    Fixed FXML file

--- src/JavaFX/Main.java
+++ src/JavaFX/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("JavaFX/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Minesweeper.fxml"));
         primaryStage.setTitle("Minesweeper");
-        primaryStage.setScene(new Scene(root, 200, 200));
+        primaryStage.setScene(new Scene(root, 400, 300));
         primaryStage.show();
     }
 
